565b4148f1ea7b1b401317bacbdc465f80484530
ShotaYoshida <dyqrks@gmail.com>
2025-12-16 10:53:14 +0900

    fix: カレントフロア設定の修正

--- src/hooks/useIdleTimer.ts
+++ src/hooks/useIdleTimer.ts
@@ -67,3 +67,4 @@ export const useIdleTimer = ({ timeout, onIdle, enabled = true }: UseIdleTimerPr
 
 
 
+
--- src/types/wsp.ts
+++ src/types/wsp.ts
@@ -62,3 +62,4 @@ export interface WspCurrentTimelineResponse {
 export type CurrentAsset = WspMedia;
 
 
+
